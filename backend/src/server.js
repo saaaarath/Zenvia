1752f1a0a171f004210ec5fbf36a26fc33828e21
saaaarath <chavanrajesh323@gmail.com>
2025-09-25 23:02:13 +0530

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import { ENV } from './config/env.js';
+
+
+const app = express();
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello world! 123');
+})
+
+
+
+app.listen(ENV.PORT, () => console.log("Server started on port:" , ENV.PORT));
