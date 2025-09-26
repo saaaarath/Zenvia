810a70c1dd453d813138f9e33bd8bd56fb0d7bdb
saaaarath <chavanrajesh323@gmail.com>
2025-09-26 22:45:37 +0530

    auth implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,21 @@
+// external imports
 import express from 'express';
+import {clerkMiddleware} from '@clerk/express';
+
+// custom imports
 import { ENV } from './config/env.js';
 import { connectDB } from './config/db.js';
+import { functions, inngest } from './config/inngest.js';
+import { serve } from 'inngest/express';
 
-
+//app setup
 const app = express();
 
+// Middleware setup
+app.use(express.json()); //req.body will be available in the request objects
+app.use(clerkMiddleware()); // req.auth will be available in the request objects
 
+app.use('/api/inngest' ,serve({ client: inngest ,functions}))
 
 app.get('/', (req, res) => {
   res.send('Hello world! 123');
@@ -13,7 +23,21 @@ app.get('/', (req, res) => {
 
 
 
-app.listen(ENV.PORT, () => {
-  console.log("Server started on port:", ENV.PORT);
-  connectDB();
-});
+const startServer = async () => { 
+  try {
+    await connectDB();
+    if (ENV.NODE_ENV !== 'production') {
+      app.listen(ENV.PORT, () => { 
+        console.log(`Server running on http://localhost:${ENV.PORT}`);
+        
+      })
+    }
+  } catch (error) {
+    console.error('Error starting server:', error);
+    process.exit(1);  // Exit the process with an error code
+  }
+}
+
+startServer();
+
+export default app;
